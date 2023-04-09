7f0120089f977f8b48b77acc17def8243f05a6c3
djaniAntova <djaniantova@gmail.com>
2023-04-09 11:03:31 +0300

    Add demo file

---
+++ 04.NodeJS-Streams-and-Utilities/streams/demo2.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const zlib = require('zlib')
+
+const gzip = zlib.createGzip()
+const readStream = fs.createReadStream('./streams/largeFile.txt', { encoding: 'utf-8' });
+const writeStream = fs.createWriteStream('./streams/copyFile.txt', { encoding: 'utf-8' })
+
+// readStream.on('data', (chunk) => {
+//     writeStream.write(chunk)
+// })
+
+// readStream.on('end', () => {
+//     writeStream.end();
+//     console.log('Finished');
+// })
+
+writeStream.on('finish', () => console.log('File is saved'))
+
+
+readStream.pipe(gzip).pipe(writeStream)
