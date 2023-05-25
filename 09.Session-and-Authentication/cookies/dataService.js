662a7cf1ee63e619c3096ee9fc6bad31974e979c
djaniAntova <djaniantova@gmail.com>
2023-05-26 00:09:09 +0300

    Add jsonwebtoken

--- 09.Session-and-Authentication/cookies/dataService.js
+++ 09.Session-and-Authentication/cookies/dataService.js
@@ -1,6 +1,9 @@
 const fs = require('fs/promises');
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
+
 const db = require('./db.json');
+const secret = 'myveryverysecretsecret';
 
 async function saveDb() {
     const data = JSON.stringify(db, null, 2) // convert object into json
@@ -8,7 +11,7 @@ async function saveDb() {
 }
 
 exports.registerUser = async (username, password) => {
-    if(db.users.some(x => x.username === username)) {
+    if (db.users.some(x => x.username === username)) {
         throw 'User already exists!'
     }
 
@@ -22,16 +25,21 @@ exports.registerUser = async (username, password) => {
 }
 
 exports.loginUser = async (username, password) => {
-    const user =  db.users.find(x=> x.username === username);
+    const user = db.users.find(x => x.username === username);
 
-    if(!user) {
+    if (!user) {
         throw 'No such username or password!'
     }
     const isAuthenticated = await bcrypt.compare(password, user.password);
-    
-    if(!isAuthenticated) {
+
+    if (!isAuthenticated) {
         throw 'No such username or password!'
     }
-    
-    return user
+    const payload = { username: user.username };
+    const options =  {expiresIn: '1h'};
+    const token = jwt.sign(payload, secret, options );
+
+    console.log(token);
+
+    return token
 }
