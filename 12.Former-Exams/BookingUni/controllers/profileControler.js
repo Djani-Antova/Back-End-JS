904593ba8b43436ff750f824ec67c3cc0ededd05
djaniAntova <djaniantova@gmail.com>
2023-06-05 20:14:56 +0300

    Edit

--- 12.Former-Exams/BookingUni/controllers/profileControler.js
+++
@@ -1,9 +0,0 @@
-const profileControler = require('express').Router();
-
-profileControler.get('/', (req, res) => {
-    res.render('profile', {
-        title: 'Profile Page'
-    })
-});
-
-module.exports = profileControler;
