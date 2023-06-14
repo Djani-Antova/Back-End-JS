579c124703ef31d5d0247cc429f4d062cc397b0d
djaniAntova <djaniantova@gmail.com>
2023-06-14 12:52:55 +0300

    Update database.js

--- 12.Former-Exams/Tutorials/config/database.js
+++ 12.Former-Exams/Tutorials/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-// TODO change database according to assignment
-const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/tutorials';
+// TODO change database 'scaffoldDb' according to assignment
+const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/scaffoldDb';
 
 
 module.exports = async (app) => {
