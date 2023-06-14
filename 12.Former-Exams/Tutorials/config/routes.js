67ccaa3c78409423f504c68126a8ae4840710f40
djaniAntova <djaniantova@gmail.com>
2023-06-14 12:55:13 +0300

    Update

--- 12.Former-Exams/Tutorials/config/routes.js
+++
@@ -1,9 +0,0 @@
-const authController = require('../controllers/authController');
-const homeControler = require('../controllers/homeControler');
-
-module.exports = (app) => {
-    app.use('/', homeControler);
-    app.use('/auth', authController);
-
-
-};
