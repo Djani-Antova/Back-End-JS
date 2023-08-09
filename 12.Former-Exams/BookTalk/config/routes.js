0921608de234293c706b9790c87ef090990e50cf
djaniAntova <djaniantova@gmail.com>
2023-08-09 10:52:36 +0300

    Update routes.js

--- 12.Former-Exams/BookTalk/config/routes.js
+++ 12.Former-Exams/BookTalk/config/routes.js
@@ -7,3 +7,25 @@ module.exports = (app) => {
 
 
 };
+// module.exports = (app) => {
+//     app.use('/', homeController);
+//     app.use('/auth', authController);
+//     app.use('/data', dataController);    
+//     app.all('*', (req, res) => {
+//         res.render('404', {
+//             title: '404 Page'
+//         } );
+//     });
+// }
+
+// const authController = require('../controllers/authController');
+// const dataController = require('../controllers/dataController');
+// const homeControler = require('../controllers/homeControler');
+// const { isLogged } = require('../middlewares/guards');
+
+// module.exports = (app) => {
+//     app.use('/', homeControler);
+//     app.use('/auth', authController); 
+//     app.use('/data', isLogged(),  dataController);
+
+// };
